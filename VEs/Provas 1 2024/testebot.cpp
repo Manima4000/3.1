507658a041acb48c91989472561888291b08cf57
Manima4000 <ferreira.matheusa@ime.eb.br>
2024-07-29 10:52:59 -0300

    .

--- VEs/Provas 1 2024/testebot.cpp
+++ VEs/Provas 1 2024/testebot.cpp
@@ -1,5 +1,5 @@
-//Alunos: Matheus ANDRADE Pinto Ferreira
-//Aluno: Gabriel Caninde de Medeiros (Reserva)
+// Alunos: Matheus ANDRADE Pinto Ferreira
+// Aluno: Gabriel Caninde de Medeiros (Reserva)
 #include <bits/stdc++.h>
 #include <unistd.h>
 
@@ -19,9 +19,16 @@ Na função gerenciar tem o delay em comentario, caso o queira adionar delay ent
 
 float delay = 1;
 
-enum Resultado_atual { Continua, Vitoria_Jogador_1, Vitoria_Jogador_2, Empate };
+enum Resultado_atual
+{
+    Continua,
+    Vitoria_Jogador_1,
+    Vitoria_Jogador_2,
+    Empate
+};
 
-class Lig4 {
+class Lig4
+{
 private:
     const int m;
     const int n;
@@ -42,11 +49,15 @@ protected:
 
     void setPontuacao2(int k) { pontuacao2 = k; }
 
-    void VerificaPontuacao(char c) {
+    void VerificaPontuacao(char c)
+    {
         // Caso de sequencia na horizontal
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n - 3; j++) {
-                if ((tab[i][j] == c) && (tab[i][j + 1] == c) && (tab[i][j + 2] == c) && (tab[i][j + 3] == c)) {
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n - 3; j++)
+            {
+                if ((tab[i][j] == c) && (tab[i][j + 1] == c) && (tab[i][j + 2] == c) && (tab[i][j + 3] == c))
+                {
                     if (c == 'X')
                         incrementaPontuacao1();
                     if (c == 'O')
@@ -56,9 +67,12 @@ protected:
         }
 
         // Caso de sequencia na vertical
-        for (int j = 0; j < n; j++) {
-            for (int i = 0; i < m - 3; i++) {
-                if ((tab[i][j] == c) && (tab[i + 1][j] == c) && (tab[i + 2][j] == c) && (tab[i + 3][j] == c)) {
+        for (int j = 0; j < n; j++)
+        {
+            for (int i = 0; i < m - 3; i++)
+            {
+                if ((tab[i][j] == c) && (tab[i + 1][j] == c) && (tab[i + 2][j] == c) && (tab[i + 3][j] == c))
+                {
                     if (c == 'X')
                         incrementaPontuacao1();
                     if (c == 'O')
@@ -68,9 +82,12 @@ protected:
         }
 
         // Caso de sequencia na diagonal descendente
-        for (int i = 0; i < m - 3; i++) {
-            for (int j = 0; j < n - 3; j++) {
-                if ((tab[i][j] == c) && (tab[i + 1][j + 1] == c) && (tab[i + 2][j + 2] == c) && (tab[i + 3][j + 3] == c)) {
+        for (int i = 0; i < m - 3; i++)
+        {
+            for (int j = 0; j < n - 3; j++)
+            {
+                if ((tab[i][j] == c) && (tab[i + 1][j + 1] == c) && (tab[i + 2][j + 2] == c) && (tab[i + 3][j + 3] == c))
+                {
                     if (c == 'X')
                         incrementaPontuacao1();
                     if (c == 'O')
@@ -80,9 +97,12 @@ protected:
         }
 
         // Caso de sequencia na diagonal ascendente
-        for (int i = 3; i < m; i++) {
-            for (int j = 0; j < n - 3; j++) {
-                if ((tab[i][j] == c) && (tab[i - 1][j + 1] == c) && (tab[i - 2][j + 2] == c) && (tab[i - 3][j + 3] == c)) {
+        for (int i = 3; i < m; i++)
+        {
+            for (int j = 0; j < n - 3; j++)
+            {
+                if ((tab[i][j] == c) && (tab[i - 1][j + 1] == c) && (tab[i - 2][j + 2] == c) && (tab[i - 3][j + 3] == c))
+                {
                     if (c == 'X')
                         incrementaPontuacao1();
                     if (c == 'O')
@@ -93,55 +113,71 @@ protected:
     }
 
 public:
-
     int getPontuacao1() { return pontuacao1; }
 
     int getPontuacao2() { return pontuacao2; }
 
-    Lig4(int m1, int n1) : m(m1), n(n1), vez(1), pontuacao1(0), pontuacao2(0) {
+    Lig4(int m1, int n1) : m(m1), n(n1), vez(1), pontuacao1(0), pontuacao2(0)
+    {
         tab = new char *[m];
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0; i < m; ++i)
+        {
             tab[i] = new char[n];
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
                 tab[i][j] = '.';
             }
         }
     }
 
-    ~Lig4() {
-        for (int i = 0; i < m; i++) {
+    ~Lig4()
+    {
+        for (int i = 0; i < m; i++)
+        {
             delete[] tab[i];
         }
         delete[] tab;
     }
 
-    void exibir() {
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+    void exibir()
+    {
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
                 cout << tab[i][j] << "  ";
             }
             cout << endl;
         }
-        cout << "\n\n" << endl;
+        cout << "\n\n"
+             << endl;
     }
 
-    bool jogadaValida(int col) {
-        if (col < 0 || col >= n) {
+    bool jogadaValida(int col)
+    {
+        if (col < 0 || col >= n)
+        {
             return false;
         }
-        if (tab[0][col] != '.') {
+        if (tab[0][col] != '.')
+        {
             return false;
         }
         return true;
     }
 
-    bool jogar(int col) {
-        if (jogadaValida(col)) {
-            for (int i = m - 1; i >= 0; i--) {
-                if (tab[i][col] == '.') {
+    bool jogar(int col)
+    {
+        if (jogadaValida(col))
+        {
+            for (int i = m - 1; i >= 0; i--)
+            {
+                if (tab[i][col] == '.')
+                {
                     tab[i][col] = (vez % 2 == 1) ? 'X' : 'O';
                     alternarVez();
                     return true;
@@ -151,9 +187,12 @@ public:
         return false;
     }
 
-    void desfazerJogada(int col) {
-        for (int i = 0; i < m; i++) {
-            if (tab[i][col] != '.') {
+    void desfazerJogada(int col)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            if (tab[i][col] != '.')
+            {
                 tab[i][col] = '.';
                 alternarVez();
                 break;
@@ -161,18 +200,24 @@ public:
         }
     }
 
-    Resultado_atual fimDeJogo() {
+    Resultado_atual fimDeJogo()
+    {
         VerificaPontuacao('X');
         VerificaPontuacao('O');
-        if (getPontuacao1() >= 1) {
+        if (getPontuacao1() >= 1)
+        {
             return Vitoria_Jogador_1;
         }
-        if (getPontuacao2() >= 1) {
+        if (getPontuacao2() >= 1)
+        {
             return Vitoria_Jogador_2;
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (tab[i][j] == '.') {
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (tab[i][j] == '.')
+                {
                     return Continua;
                 }
             }
@@ -180,19 +225,25 @@ public:
         return Empate;
     }
 
-    void gerenciar() {
+    void gerenciar()
+    {
         //sleep(delay); // Caso queira adicionar delay
-        srand(time(0));  // Inicializa a semente do gerador de números aleatórios
+        srand(time(0)); // Inicializa a semente do gerador de números aleatórios
         exibir();
-        while (fimDeJogo() == Continua) {
-            if (vez % 2 == 1) {
-                int melhorColuna = melhorJogada(5);  // Profundidade 5 para o minimax
+        while (fimDeJogo() == Continua)
+        {
+            if (vez % 2 == 1)
+            {
+                int melhorColuna = melhorJogada(5); // Profundidade 5 para o minimax
                 jogar(melhorColuna);
                 cout << "Jogador 1 (Bot) jogou na coluna " << melhorColuna + 1 << endl;
-            } else {
+            }
+            else
+            {
                 int col;
-                do {
-                    col = rand() % 7;  // Jogada aleatória entre as 7 colunas
+                do
+                {
+                    col = rand() % 7; // Jogada aleatória entre as 7 colunas
                 } while (!jogadaValida(col));
                 jogar(col);
                 cout << "Jogador 2 jogou na coluna " << col + 1 << endl;
@@ -201,21 +252,29 @@ public:
         }
 
         Resultado_atual resultado = fimDeJogo();
-        if (resultado == Vitoria_Jogador_1) {
+        if (resultado == Vitoria_Jogador_1)
+        {
             cout << "Jogador 1 venceu !!" << endl;
-        } else if (resultado == Vitoria_Jogador_2) {
+        }
+        else if (resultado == Vitoria_Jogador_2)
+        {
             cout << "Jogador 2 venceu !!" << endl;
-        } else {
+        }
+        else
+        {
             cout << "Empate" << endl;
         }
     }
 
-    void resetarTabuleiro() {
+    void resetarTabuleiro()
+    {
         vez = 1;
         setPontuacao1(0);
         setPontuacao2(0);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
                 tab[i][j] = '.';
             }
         }
@@ -226,18 +285,23 @@ public:
     int avaliar();
 };
 
-class Lig4Tradicional : public Lig4 {
+class Lig4Tradicional : public Lig4
+{
 public:
     Lig4Tradicional() : Lig4(6, 7) {}
 };
 
-int Lig4::avaliar() {
+int Lig4::avaliar()
+{
     int score = 0;
 
     // Centralidade
-    for (int i = 0; i < m; i++) {
-        if (tab[i][n / 2] == 'X') score += 3;
-        if (tab[i][n / 2] == 'O') score -= 3;
+    for (int i = 0; i < m; i++)
+    {
+        if (tab[i][n / 2] == 'X')
+            score += 3;
+        if (tab[i][n / 2] == 'O')
+            score -= 3;
     }
 
     // Adicionar aqui outras heurísticas de pontuação para sequências parciais
@@ -252,38 +316,49 @@ int Lig4::avaliar() {
     return score;
 }
 
-int Lig4::minimax(int profundidade, bool isMaximizingPlayer, int alpha, int beta) {
+int Lig4::minimax(int profundidade, bool isMaximizingPlayer, int alpha, int beta)
+{
     Resultado_atual resultado = fimDeJogo();
-    if (resultado != Continua || profundidade == 0) {
+    if (resultado != Continua || profundidade == 0)
+    {
         return avaliar();
     }
 
-    if (isMaximizingPlayer) {
+    if (isMaximizingPlayer)
+    {
         int maxEval = INT_MIN;
-        for (int col = 0; col < n; col++) {
-            if (jogadaValida(col)) {
+        for (int col = 0; col < n; col++)
+        {
+            if (jogadaValida(col))
+            {
                 jogar(col);
                 int eval = minimax(profundidade - 1, false, alpha, beta);
                 desfazerJogada(col);
                 maxEval = max(maxEval, eval);
                 alpha = max(alpha, eval);
-                if (beta <= alpha) {
-                    break;  // Poda beta
+                if (beta <= alpha)
+                {
+                    break; // Poda beta
                 }
             }
         }
         return maxEval;
-    } else {
+    }
+    else
+    {
         int minEval = INT_MAX;
-        for (int col = 0; col < n; col++) {
-            if (jogadaValida(col)) {
+        for (int col = 0; col < n; col++)
+        {
+            if (jogadaValida(col))
+            {
                 jogar(col);
                 int eval = minimax(profundidade - 1, true, alpha, beta);
                 desfazerJogada(col);
                 minEval = min(minEval, eval);
                 beta = min(beta, eval);
-                if (beta <= alpha) {
-                    break;  // Poda alfa
+                if (beta <= alpha)
+                {
+                    break; // Poda alfa
                 }
             }
         }
@@ -291,17 +366,21 @@ int Lig4::minimax(int profundidade, bool isMaximizingPlayer, int alpha, int beta
     }
 }
 
-int Lig4::melhorJogada(int profundidade) {
+int Lig4::melhorJogada(int profundidade)
+{
     int melhorValor = INT_MIN;
     int melhorColuna = -1;
 
-    for (int col = 0; col < n; col++) {
-        if (jogadaValida(col)) {
+    for (int col = 0; col < n; col++)
+    {
+        if (jogadaValida(col))
+        {
             jogar(col);
             int moveValue = minimax(profundidade - 1, false, INT_MIN, INT_MAX);
             desfazerJogada(col);
 
-            if (moveValue > melhorValor) {
+            if (moveValue > melhorValor)
+            {
                 melhorValor = moveValue;
                 melhorColuna = col;
             }
@@ -310,22 +389,24 @@ int Lig4::melhorJogada(int profundidade) {
     return melhorColuna;
 }
 
-int main() {
+int main()
+{
     int partidas_jogadas = 0;
     Lig4Tradicional jogo;
-    while (true) {
+    while (true)
+    {
         jogo.gerenciar();
         partidas_jogadas++;
         cout << "Partidas jogadas: " << partidas_jogadas << endl;
 
         Resultado_atual resultado = jogo.fimDeJogo();
-        if (resultado == Vitoria_Jogador_2 || jogo.getPontuacao2() >= 1) {
+        if (resultado == Vitoria_Jogador_2 || jogo.getPontuacao2() >= 1)
+        {
             cout << "Jogador 2 venceu uma partida!!" << endl;
             break;
         }
 
         jogo.resetarTabuleiro();
-        
     }
     return 0;
 }
